0c5204cea3b873615c8036fbc0ddeac21268ab74
Joe Panuncialman <joseph.a.panuncialman@vanderbilt.edu>
2017-12-14 16:49:42 -0500

    Second commit.

--- app.js
+++ app.js
@@ -1,13 +1,150 @@
+//Requiring Express and setting up variables.
 var express = require('express');
-
 var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+var formidable = require('formidable');
+var credentials = require('./credentials.js');
+var session = require('express-session');
+var parseurl = require('parseurl');
 
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(session({
+	resave: false,
+	saveUnintialized: true,
+	secret: credentials.cookieSecret
+}));
+
+//For Secuirty
+app.disable('x-powered-by');
+
+
+//Port Listening
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res){
-	res.send('Express Works!');
+//Setting up static directory.
+app.use(express.static(__dirname + '/public'));
+
+
+//Session Handling
+app.use(function(req, res, next){
+	var views = req.session.views;
+
+	if (!views) {
+		views = req.session.views = {};
+	}
+
+	var pathname = parseurl(req).pathname;
+
+	views[pathname] = (views[pathname] || 0) + 1;
+
+	next();
 });
+
+app.get('/viewcount', function(req, res, next){
+	res.send('You viewed this page ' + req.session.views['/viewcount'] + ' times.');
+});
+
+/** RENDERING VIEWS **/
+
+//Setting up pages
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.use(function(req, res, next){
+	console.log("Looking for URL: " + req.url);
+	next();
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+//Contact Form
+app.get('/contact', function(req, res){
+	res.render('contact', {csrf: 'CSRF token here'});
+});
+
+app.get('/thankyou', function(req, res){
+	res.render('thankyou');
+})
+
+app.post('/process', function(req, res){
+	console.log('Form: ' + req.query.form);
+	console.log('CSRF token: ' + req.body._csrf);
+	console.log('Email: ' + req.body.email);
+	console.log('Question: ' + req.body.ques);
+
+	res.redirect(303, '/thankyou');
+});
+
+//File Upload
+app.get('/file-upload', function(req, res){
+	var now = new Date();
+	res.render('file-upload', {
+		year: now.getFullYear(),
+		month: now.getMonth() 
+	});
+});
+
+app.post('/file-upload/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, file){
+		if(err)
+			return res.redirect(303, '/error');
+		console.log('Received file!');
+		console.log(file);
+		res.redirect(303, '/thankyou');
+
+	})
+});
+
+app.get('/error', function(req, res) {
+	res.render('error');
+})
+
+//Cookie Handling
+app.get('/cookie', function(req, res){
+	res.cookie('username', 'Joe Panuncialman', {expire: new Date() + 9999}).send('username has the value of Joe Panuncialman');
+});
+
+app.get('/listcookies', function(req, res){
+	console.log("Cookies: ", req.cookies);
+	res.send('Look in the console for cookies.');
+});
+
+app.get('/deletecookie', function(req, res){
+	res.clearCookie('username');
+	res.send('username Cookie Deleted');
+})
+
+
+
+
+
+//404, 500 Errors
+app.use(function(req, res){
+	res.type('text/html');
+	res.status(404);
+	res.render('404');
+})
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
+
+
 
 app.listen(app.get('port'), function(){
 	console.log("Listening on port " + app.get('port'));
 });
+
+
